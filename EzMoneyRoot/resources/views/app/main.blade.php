dd7e0339db133cbfd99d37d4b023f4c2daef4fdb
JohnVRS <joaovitorrodriguessantos8@outlook.com>
2024-08-27 22:35:24 -0300

    aplicando css no container-overview

--- EzMoneyRoot/resources/views/app/main.blade.php
+++ EzMoneyRoot/resources/views/app/main.blade.php
@@ -2,40 +2,49 @@
 @section('title', 'Homepage')
 @section('content')
 
-  <div class="container-overview">
+<div class="container-section">
+    <div class="container-overview">
     
-      <div class="welcome">
-        <small style="font-size: 18px;color: rgb(82,82,82);">Boa tarde, -User-</small><br>  
-      </div>
-
-      <div class="budget-overview">
-          <div class="revenue">
-              <p>Receita Mensal</p>
-          </div>
-
-          <div class="expense">
-              <p>Despesa Mensal</p>
-          </div>
-
-          <div class="report">
-              <p>Ver relátorios</p>
-          </div>
-      </div>
-
-      <div class="quick-actions">
-          <div class="label">
-            <h3>Acesso rápido</h3>
-          </div>
-
-          <button class="btn-expense">Despesa</button>
-
-          <button class="btn-revenue">Receita</button>
-
-          <button class="btn-transfer">Transferência</button>
-
-          <button class="btn-export-excel">Exportar</button>
-      </div>
-  </div>
+        <div class="box-leftside">
+            <div class="greeting">
+                 <p>Boa noite,</p>
+                 <p>João Vitor Rodrigues Santos</p>
+            </div>
+        
+            <div class="budget-overview">
+                <div class="budget-blocks" id="revenue">
+                    <label>receita mensal</label>
+                    <strong style="color: #129e3f;">R$ 5.651,47</strong>
+                </div>
+      
+                <div class="budget-blocks" id="expense">
+                  <label>despesa mensal</label>
+                  <strong style="color: #d72638;">R$ 2.231,17</strong>
+                </div>
+      
+                <div class="budget-blocks" id="report">
+                    <p>ver relátorios</p>
+                </div>  
+            </div>
+        </div>
+        <div class="box-rightside">
+            <div class="quick-actions">
+                <div class="label">
+                  <p>Acesso rápido</p>
+                </div>
+      
+                <button class="btn-expense">Despesa</button>
+      
+                <button class="btn-revenue">Receita</button>
+      
+                <button class="btn-transfer">Transferência</button>
+      
+                <button class="btn-export-excel">Exportar</button>
+            </div>
+        </div>
+    </div>
+</div>
+  
   
  <a href="{{route('login.logout')}}">LOGOUT</a>
   @endsection
