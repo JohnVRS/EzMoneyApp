b28323e321123d7d4a056a6d8c9e2e3ef15a0100
JohnVRS <joaovitorrodriguessantos8@outlook.com>
2025-02-19 21:40:53 -0300

    Estrutura html do modal com bootstrap

--- EzMoneyRoot/resources/views/app/main.blade.php
+++ EzMoneyRoot/resources/views/app/main.blade.php
@@ -64,15 +64,53 @@
                     </div>
 
                     <!-- Estrutura do Modal -->
-                    <div class="modal fade" id="test" tabindex="-1" aria-hidden="true">
+                    <div class="modal fade" id="expenseModal" tabindex="-1" aria-hidden="true">
                         <div class="modal-dialog modal-dialog-centered">
+                            
                             <div class="modal-content">
-                                <div class="modal-header">
-                                    <h1>TESTE</h1>
+                                <div class="button-wrapper">
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="close"></button>
                                 </div>
-                                <div class="modal-body"></div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="close">TESTe</button>
+                                
+                                <div class="header">
+                                    <h5> Nova Despesa</h3>
+                                </div>
+                                <div class="body">
+                                    <div class="inputRow">
+                                        <label for="">Descrição</label>
+                                        <div class="groupField">
+                                            <input type="text" name="desc" id="input-modal-desc">
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="inputRow">
+                                        <label for="">Valor</label>
+                                        <div class="groupField">
+                                            <input type="number">
+                                        </div>
+                                   </div>
+                                    
+                                    <div class="inputRow">
+                                        <label for="">Data</label>
+                                        <div class="groupField">
+                                            <input type="date" name="" id="">
+                                        </div>
+                                    </div>
+                                    <div class="inputRow">
+                                        <label for="">Conta/Cartão</label>
+                                        <div class="groupField">
+                                            <input type="radio" name="" id="">
+                                        </div>
+
+                                        <label for="">Categoria</label>
+                                        <div class="groupField">
+                                            <input type="radio" name="" id="">
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                                <div class="footer">
+                                    
                                 </div>
                             </div>
                         </div>
