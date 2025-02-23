7535422b5f57f63e4c86db44e6b940342c6e6fa5
JohnVRS <joaovitorrodriguessantos8@outlook.com>
2025-02-23 12:50:03 -0300

    grid no modal + botão de save

--- EzMoneyRoot/resources/views/components/modal/revenue.blade.php
+++ EzMoneyRoot/resources/views/components/modal/revenue.blade.php
@@ -10,7 +10,7 @@
             <div class="header">
                 <h5> Nova Receita</h3>
             </div>
-            <div class="body">
+            <div class="container-fluid">
                 <div class="inputRow">
                     <label for="">Descrição</label>
                     <div class="groupField">
@@ -18,41 +18,64 @@
                     </div>
                 </div>
                  
-                <div class="inputRow">
-                    <label for="">Valor</label>
-                    <div class="groupField">
-                        <input type="number">
+                <div class="row">
+                    <div class="col-6">
+
+                        <label for="">Valor</label>
+                        <div class="groupField">
+                            <input type="number">
+                        </div>
                     </div>
-                    <label for="">Data</label>
-                    <div class="groupField">
-                        <input type="date" name="" id="">
+                    
+                    <div class="col-6">
+
+                        <label for="">Data</label>
+                        <div class="groupField">
+                            <input type="date" name="" id="">
+                        </div>
                     </div>
                 </div>
+
                 <div class="inputRow">
-                    <label for="">Conta/Cartão</label>
-                    <div class="groupField">
-                        <select name="selectCard" id="selectCard">
-                            <option selected> Escolha o cartão...</option>
-                            <option value="Mercado Pago">Mercado Pago</option>
-                            <option value="Bradesco"> Bradesco</option>
-                            <option value="Nubank">Nubank</option>
-                        </select>
+                
+                    <div class="row">
+                        <div class="col">
+
+                            <label for="">Conta/Cartão</label>
+                            <div class="groupField">
+                                <select name="selectCard" id="selectCard">
+                                    <option selected> Escolha o cartão...</option>
+                                    <option value="Mercado Pago">Mercado Pago</option>
+                                    <option value="Bradesco"> Bradesco</option>
+                                    <option value="Nubank">Nubank</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="col">
+
+                            <label for="">Categoria</label>
+                            <div class="groupField">
+                                <select name="selectCategory" id="selectCategory">
+                                    <option selected> Buscar Categoria...</option>
+                                    <option value="1">Alimentação</option>
+                                    <option value="2"> Saúde</option>
+                                    <option value="3">Familía</option>
+                                    <option value="4">Lazer</option>
+                                </select>
+                            </div>
+                        </div>
                     </div>
-  
-                    <label for="">Categoria</label>
-                    <div class="groupField">
-                        <select name="selectCategory" id="selectCatefory">
-                            <option selected> Buscar Categoria...</option>
-                            <option value="1">Alimentação</option>
-                            <option value="2"> Saúde</option>
-                            <option value="3">Familía</option>
-                            <option value="4">Lazer</option>
-                        </select>
-                    </div>
+                    
                 </div>
                 
             </div>
             <div class="footer">
+                <div class="button-wrapper">
+                    <button class="btn">
+                        <i class="bi bi-check-lg text-light"></i>
+                    </button>
+                </div>
                 
             </div>
         </div>
